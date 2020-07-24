abbd8bc1d5ca8e8330a3c5a26592494a0eb1c8f5
SergeLeon <g605413@gmail.com>
2020-07-24 14:05:14 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -7,12 +7,12 @@ if not os.path.exists("Files"):  # Checking for the "Files" directory
 
 while True:
     url = 'http://static.donationalerts.ru/audiodonations/'  # Generating a download link
-    url1 = "6" + str(random.randrange(4, 6)) + str(random.randrange(111, 999))
+    url1 = "6{0}{1}".format(str(random.randrange(4, 6)), str(random.randrange(111, 999)))
     url2 = str(random.randrange(111, 999))
-    url = url + url1 + '/' + url1 + url2 + ".wav"
+    url = "{0}{1}/{1}{2}.wav".format(url, url1, url2)
     req = requests.get(url)
     stat = req.status_code
     if stat == 200:  # Access check
-        print(url)
+        print("Есть подключение {0}".format(url))
         myFile = requests.get(url, allow_redirects=True)
-        open(os.path.join("Files" , url1 + "_" + url1 + url2 + ".wav"), 'wb').write(myFile.content)  # Save file
+        open(os.path.join("Files/{0}_{0}{1}.wav".format(url1, url2)), 'wb').write(myFile.content)  # Save file
