6804302caeb6e33116e8120624ba73dedf9ff571
SergeLeon <g605413@gmail.com>
2020-08-15 12:51:26 +0300

    Update

--- Parser.py
+++ Parser.py
@@ -2,17 +2,28 @@ import os
 import requests
 import random
 
-if not os.path.exists("Files"):  # Checking for the "Files" directory
-    os.mkdir("Files")  # Creating the "Files" directory if it is not available
 
-while True:
-    url = 'http://static.donationalerts.ru/audiodonations/'  # Generating a download link
+def generate_random_DA_url():
+    url = 'http://static.donationalerts.ru/audiodonations/'
     url1 = "6{0}{1}".format(str(random.randrange(4, 6)), str(random.randrange(111, 999)))
     url2 = str(random.randrange(111, 999))
     url = "{0}{1}/{1}{2}.wav".format(url, url1, url2)
-    req = requests.get(url)
+    return url
+
+
+def save_DA_audio(url):
+    filecontent = requests.get(url, allow_redirects=True)
+    filename = url[-18:-4].replace('/', '_')
+    open("Files/{0}.wav".format(filename), 'wb').write(filecontent.content)
+
+
+if not os.path.exists("Files"):
+    os.mkdir("Files")
+
+while True:
+    fileurl = generate_random_DA_url()
+    req = requests.get(fileurl)
     stat = req.status_code
-    if stat == 200:  # Access check
-        print("Есть подключение {0}".format(url))
-        myFile = requests.get(url, allow_redirects=True)
-        open(os.path.join("Files/{0}_{0}{1}.wav".format(url1, url2)), 'wb').write(myFile.content)  # Save file
+    if stat == 200:
+        print("Conected {0}".format(fileurl))
+        save_DA_audio(fileurl)
